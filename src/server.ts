0427c1123cd1beb7f6152ce6afa39afabd1449a1
ViniciusNF <viniciusnevesfaria@gmail.com>
2025-07-10 22:34:09 -0300

    feat: add create question, create room and get room questions

--- src/server.ts
+++ src/server.ts
@@ -1,30 +1,36 @@
-import { fastify } from 'fastify'; 
-import {fastifyCors } from '@fastify/cors';
+import { fastify } from 'fastify';
+import { fastifyCors } from '@fastify/cors';
 import {
-    serializerCompiler,
-    validatorCompiler,
-    type ZodTypeProvider,
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
 } from 'fastify-type-provider-zod';
 // import { sql } from './db/connection.ts';
 import { env } from './env.ts';
 import { getRoomsRoute } from './http/routes/get-rooms.ts';
+import { createRoomsRoute } from './http/routes/create-room.ts';
+import { getRoomQuestions } from './http/routes/get-room-questions.ts';
+import { createQuestionRoute } from './http/routes/create-questions.ts';
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
 app.register(fastifyCors, {
-    origin: 'http://localhost:5173',
+  origin: 'http://localhost:5173',
 });
 
 app.setSerializerCompiler(serializerCompiler);
 app.setValidatorCompiler(validatorCompiler);
 
 app.get('/health', () => {
-    return 'OK'
-})
+  return 'OK';
+});
 
-app.register(getRoomsRoute)
+app.register(getRoomsRoute);
+app.register(createRoomsRoute);
+app.register(getRoomQuestions);
+app.register(createQuestionRoute);
 
-app.listen({ port: env.PORT })
+app.listen({ port: env.PORT });
 // .then(() => {
 //     console.log(`Port: ${process.env.PORT}`);
 //     console.log('HTTP server running!!!!!!')
